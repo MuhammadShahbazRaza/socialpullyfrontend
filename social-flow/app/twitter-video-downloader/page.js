b7a74b3343987afd81a7300ab4e1867d658e6c4b
Shahbaz Rizvi <laptopc@192.168.10.7>
2026-01-06 23:37:10 +0500

    issue

--- social-flow/app/twitter-video-downloader/page.js
+++ social-flow/app/twitter-video-downloader/page.js
@@ -1,7 +1,21 @@
-import DownloadForm from '@/components/DownloadForm';
+import DownloadForm from "@/components/DownloadForm";
 
 export const metadata = {
-  title: 'Twitter Video Downloader - Download Twitter/X Videos & GIFs Free',
-  description: 'Free Twitter (X) video downloader. Save Twitter videos, GIFs in HD quality. Fast and easy Twitter media download.',
-  keywords: ['twitter video downloader', 'download twitter videos', 'x video downloader', 'twitter gif download']
+  title: "Twitter Video Downloader - Download Twitter/X Videos & GIFs Free",
+  description:
+    "Free Twitter (X) video downloader. Save Twitter videos and GIFs in HD quality. Fast and easy Twitter media download.",
+  keywords: [
+    "twitter video downloader",
+    "download twitter videos",
+    "x video downloader",
+    "twitter gif download",
+  ],
 };
+
+export default function TwitterVideoDownloaderPage() {
+  return (
+    <main>
+      <DownloadForm platform="twitter" />
+    </main>
+  );
+}
